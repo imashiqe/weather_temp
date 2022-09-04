549db5674fa4c9be68ff9fc71ae28afd01242af1
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-04 16:42:29 +0600

    temparature added

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const API_key = `7c25e23e1b644891326742d68d70ab46`;
 
 const loadTemperature = city => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`;
     fetch(url)
     .then(res => res.json())
     .then(data => displayTemperature(data));
@@ -14,5 +14,11 @@ const displayTemperature = data  => {
      temperature.innerText = data.main.temp;
 }
 
+
+document.getElementById('btn-search').addEventListener('click', function(){
+    const searchField = document.getElementById('search-field');
+    const city = searchField.value;
+    loadTemperature(city);
+})
 
 loadTemperature('dhaka');
