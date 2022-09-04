862c726a09a956766a1a8bc68e1a4e1a0944b9b6
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-04 17:35:18 +0600

    clouds data load

--- js/app.js
+++ js/app.js
@@ -9,15 +9,27 @@ const loadTemperature = city => {
 }
 
 const displayTemperature = data  => {
-    const temperature = document.getElementById('temperature');
-    console.log(data.main.temp);
-     temperature.innerText = data.main.temp;
+    // const temperature = document.getElementById('temperature');
+   
+    //  temperature.innerText = data.main.temp;
+    setInnerTextById('temperature' , data.main.temp);
+    setInnerTextById('condition' , data.weather[0].main);
+     console.log(data.weather[0].main);
 }
+
+
+const setInnerTextById  = (id, text) => {
+    const temperature = document.getElementById(id);
+    temperature.innerText = text;
+}
+
 
 
 document.getElementById('btn-search').addEventListener('click', function(){
     const searchField = document.getElementById('search-field');
     const city = searchField.value;
+    //set city
+    document.getElementById('city').innerText = city;
     loadTemperature(city);
 })
 
